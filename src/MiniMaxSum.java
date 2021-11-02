9f55412f8ea90efd0bc50c50f63f9b3f18922b23
İsmail Özcan <l5215067@std.yildiz.edu.tr>
2021-11-02 23:59:20 +0300

    MiniMaxSum added

---
+++ src/MiniMaxSum.java
@@ -0,0 +1,21 @@
+import java.util.Collections;
+import java.util.List;
+
+public class MiniMaxSum {
+
+    public static void miniMaxSum(List<Integer> arr) {
+        // Write your code here
+        Collections.sort(arr);
+        long min=0;
+        long max=0;
+        for(int i=0;i<arr.size()-1;i++){
+            min+=arr.get(i);
+        }
+        for(int i=1;i<arr.size();i++){
+            max+=arr.get(i);
+        }
+
+        System.out.print(min +" "+max );
+
+    }
+}
